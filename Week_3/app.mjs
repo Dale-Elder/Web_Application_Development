1fc80a9186ab234cc59475ff28d5f318c0f35c6a
Dale <66420874+Dale-Elder@users.noreply.github.com>
2023-04-09 21:34:17 +0100

    refactored and setup index.html, index.js into public folder, and created basic stylesheet for the index.html.

--- Week_3/app.mjs
+++ Week_3/app.mjs
@@ -5,11 +5,13 @@ import Database from "better-sqlite3";
 
 // app express() is a function that creates an express application
 const app = express();
+
 // Create a database connection to the database file
 const db = new Database("C:\\Users\\felix\\wadsongs.db");
 
 // app.use(express.static("public")); to serve static files
 app.use(express.static("public"));
+
 // app.use(express.json()); to parse JSON bodies
 app.use(express.json());
 
---
+++ Week_3/public/index.js
@@ -0,0 +1,3 @@
+console.log("Hello from client-side JavaScript!");
+
+const artist = document.getElementById("artist").value;
