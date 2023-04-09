179ac8fe0b708db73846ddad9d31ce6060f07452
Dale <66420874+Dale-Elder@users.noreply.github.com>
2023-04-09 23:03:09 +0100

    refactored and added comments to index.js.

--- Week_3/public/index.js
+++ Week_3/public/index.js
@@ -1,8 +1,8 @@
-console.log("Hello from client-side JavaScript!");
+// console.log("Hello from client-side JavaScript!");
 
 // Make the AJAX run when we click a button
 document.getElementById("ht_search").addEventListener("click", (e) => {
-  // Read te artist from the input field
+  // Read the artist from the input field
   const artist = document.getElementById("ht_artist").value;
   ajaxSearch(artist);
 });
@@ -20,15 +20,18 @@ async function ajaxSearch(artist) {
     songs.forEach((song) => {
       html += `Artist: ${song.artist}, Title: ${song.title}, Year: ${song.year}<br />`;
     });
-    console.log(html);
+    // console.log(html);
 
+    // update the HTML
     document.getElementById("results").innerHTML = html;
   } catch (error) {
     alert(`There was an error: ${error}`);
   }
 }
 
+// Make the AJAX run when we click a button
 document.getElementById("ht_add").addEventListener("click", async () => {
+  // Read new song details from the input fields
   const newSong = {
     title: document.getElementById("title").value,
     artist: document.getElementById("artist").value,
@@ -38,21 +41,27 @@ document.getElementById("ht_add").addEventListener("click", async () => {
     quantity: document.getElementById("quantity").value,
   };
 
+  // Send the new song to the server using AJAX POST, as JSON
   try {
     const response = await fetch("/add/song", {
       method: "POST",
       headers: {
         "Content-Type": "application/json",
       },
+      // convert the JS object to JSON
       body: JSON.stringify(newSong),
     });
+    // if the request is successful, display a success message
     if (response.status === 200) {
       alert("Song added successfully!");
+      // if the request is unsuccessful and display status 400, display a message
     } else if (response.status === 400) {
       alert("blank fields are not allowed");
+      // if the request is unsuccessful and display a message with the response status
     } else {
       alert(`Uknown error: code ${response.status}`);
     }
+    // catch any errors and display them
   } catch (error) {
     alert(`There was an error: ${error}`);
   }
